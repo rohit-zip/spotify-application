26bb462c3af45629f6075998a1b82362849b7bfa
Rohit <rohitparih@gmail.com>
2024-09-08 14:32:09 +0530

    Feature -> Splash Page redirect to Get Started Page

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify/core/configs/assets/app_vector.dart';
+import 'package:spotify/presentation/intro/pages/get_started.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +24,16 @@ class Splash extends StatelessWidget {
       body: Center(
         child: SvgPicture.asset(AppVectors.logo),
       ),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+    context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => const GetStarted(),
+      )
     );
   }
 }
